5edc3e06b352fe98664d80ffbd39dd821cf41cf2
nat-s-klein <natalie.klein@codesmith.io>
2023-03-27 17:03:46 -0600

    make first styled component Container,  add gradient

--- src/components/background.js
+++ src/components/background.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from 'styled-components'
 
 export default function Background () {
     return (
-        <hi>im the Background</hi>
+        <div>
+            <Container>hey</Container>
+        </div>
     )
 }
+
+// Styled Components 
+
+const Container = styled.div `
+    padding: 0.25em 1em;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: #3494E6;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #EC6EAD, #3494E6);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #EC6EAD, #3494E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`
