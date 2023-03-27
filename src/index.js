7552e706c4015d31478f1da94b92cba0ce8ed45a
nat-s-klein <natalie.klein@codesmith.io>
2023-03-27 16:20:32 -0600

    send axios requests to newly creates server routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-// require('./index.css'); 
 import axios from 'axios'
-
-const url = 'http://localhost:3000/hi'
 
 const App = () => {
 
-  axios.get(url).then((res) => console.log('res is:', res)).catch((err) => console.log(err))
+  axios.get('http://localhost:3000/fromdb').then((res) => console.log('res is:', res)).catch((err) => console.log(err))
+
+  axios.get('http://localhost:3000/languages').then((res) => console.log('res is:', res)).catch((err) => console.log(err))
 
   return (
     <div>
-      <h1>hola!!</h1>
+      <h1>hojkjnla!!</h1>
     </div>
   );
 };
